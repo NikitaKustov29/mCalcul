a83e6dfaee6f02fb137516172b6b03e8ec3857af
NikitaKustov29 <114473572+NikitaKustov29@users.noreply.github.com>
2022-10-18 10:20:43 +0300

    я сделал калькулятор

---
+++ ван_пр/Program.cs
@@ -0,0 +1,52 @@
+﻿
+using System.Diagnostics;
+
+namespace training;
+
+class Program
+{
+    static void Main()
+    {
+        Calc();
+    }
+
+    static void Calc()
+    {
+        double n1, n2;
+        double res = 0;
+        string zn;
+        Console.WriteLine("введите первое число: ");
+
+        n1 = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("введите второе число: ");
+
+        n2 = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Выберите действие: +, -, /, *");
+
+        zn = Console.ReadLine();
+
+        if (zn == "+")
+        {
+            res += n1 + n2;
+            Console.WriteLine("Результат: " + res);
+        }
+        else if (zn == "-")
+        {
+            res += n1 - n2;
+            Console.WriteLine("Результат: " + res);
+        }
+        else if (zn == "/")
+        {
+            res += n1 / n2;
+            Console.WriteLine("Результат: " + res);
+        }
+        else if (zn == "*")
+        {
+            res += n1 * n2;
+            Console.WriteLine("Результат: " + res);
+        }
+    }
+}
+
